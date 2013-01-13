e8fbfcb2a97e84184c696ae71a32da34cb4b7cea
SeanJA <sean.sandy@gmail.com>
2013-01-13 15:18:43 -0400

    getCustomDelimiters function

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -1,53 +1,59 @@
 <?php
 
-class StringCalculator{
-    function add($string){
+class StringCalculator {
+
+    function add($string) {
         $array = $this->buildNumberArray($string);
         $array = $this->checkForNumbersGreaterThan1000($array);
         $this->checkForNegatives($array);
         $total = array_sum($array);
         return $total;
     }
-    
-    private function buildNumberArray($string){
+
+    private function buildNumberArray($string) {
         $matches = array();
-        if(preg_match("/^\/\/\[?(.*?)\]?\\n/", $string, $matches)){
-            $delimiter = $matches[1];
+        $delimiters = ',|\n';
+        if (preg_match("/^\/\/\[?(.*?)\]?\\n/", $string, $matches)) {
+            $delimiters = $this->getCustomDelimiters($matches[1]);
             $count = 1;
             $string = str_replace($matches[0], '', $string, $count);
-            if(strpos($delimiter, '][') !== false){
-                $delimiter = explode('][', $delimiter);
-                array_walk($delimiter, 'preg_quote');
-                $array = preg_split("/[".implode('|', $delimiter)."]/", $string);
-            } else {
-                $array = explode($delimiter, $string);
-            }
-        } else {
-            $array = preg_split("/[,|\n]/", $string);
         }
+        $array = preg_split("/[" . $delimiters . "]/", $string);
         return $array;
     }
-    
-    private function checkForNumbersGreaterThan1000($array){
-        foreach($array as &$a){
-            if($a > 1000){
+
+    private function getCustomDelimiters($delimiters) {
+        if (strpos($delimiters, '][') !== false) {
+            $delimiters = explode('][', $delimiters);
+            array_walk($delimiters, 'preg_quote');
+            $delimiters = implode('|', $delimiters);
+        }
+        return $delimiters;
+    }
+
+    private function checkForNumbersGreaterThan1000($array) {
+        foreach ($array as &$a) {
+            if ($a > 1000) {
                 $a = 0;
             }
         }
         return $array;
     }
-    
-    private function checkForNegatives($array){
+
+    private function checkForNegatives($array) {
         $negatives = array();
-        foreach($array as $s){
-            if($s < 0){
+        foreach ($array as $s) {
+            if ($s < 0) {
                 $negatives[] = $s;
             }
         }
-        if(!empty($negatives)){
+        if (!empty($negatives)) {
             throw new StringCalculatorNegativeNumberException('negatives not allowed ' . implode(',', $negatives));
         }
     }
+
 }
 
-class StringCalculatorNegativeNumberException extends Exception{}
+class StringCalculatorNegativeNumberException extends Exception {
+    
+}
