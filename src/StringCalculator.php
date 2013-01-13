6f97e363cf958ce711588c5b4909bddb89d3e9d5
SeanJA <sean.sandy@gmail.com>
2013-01-13 14:49:23 -0400

    checkForNegatives in its own function

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -7,12 +7,18 @@ class StringCalculator{
             $delimiter = $matches[1];
             $count = 1;
             $string = str_replace($matches[0], '', $string, $count);
-            $string = explode($delimiter, $string);
+            $array = explode($delimiter, $string);
         } else {
-            $string = preg_split("/[,|\n]/", $string);
+            $array = preg_split("/[,|\n]/", $string);
         }
+        $this->checkForNegatives($array);
+        $total = array_sum($array);
+        return $total;
+    }
+    
+    private function checkForNegatives($array){
         $negatives = array();
-        foreach($string as $s){
+        foreach($array as $s){
             if($s < 0){
                 $negatives[] = $s;
             }
@@ -20,8 +26,6 @@ class StringCalculator{
         if(!empty($negatives)){
             throw new StringCalculatorNegativeNumberException('negatives not allowed ' . implode(',', $negatives));
         }
-        $total = array_sum($string);
-        return $total;
     }
 }
 
